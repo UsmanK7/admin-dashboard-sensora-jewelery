13f01ff291ccaab5635c18e728c6f371154c5c62
UsmanK7 <[200~musmankhan707@gmail.com>
2023-10-11 03:21:10 +0500

    fefe

--- assets/Js/app.js
+++ assets/Js/app.js
@@ -1,10 +1,10 @@
-$('.menu-icon').on('click', function () {
-    $('.sidebar').css('left', '0px');
-    $('.overlay').css('display', 'block');
-});
-$('.overlay').on('click', function () {
-    $('.sidebar').css('left', '-300px');
-    $('.overlay').css('display', 'none');
+document.addEventListener("DOMContentLoaded", function () {
+    // Add 'active' class to the first tab and set initial line position
+    const initialTab = document.querySelector('.tab_btn');
+    initialTab.classList.add('active');
+    const line = document.querySelector('.line');
+    line.style.width = initialTab.offsetWidth + "px";
+    line.style.left = initialTab.offsetLeft + "px";
 });
 
 const tabs = document.querySelectorAll('.tab_btn');
@@ -17,8 +17,16 @@ tabs.forEach((tab, index) => {
         var line = document.querySelector('.line');
         line.style.width = e.target.offsetWidth + "px";
         line.style.left = e.target.offsetLeft + "px";
-        all_content.forEach(content=>{content.classList.remove('active')});
+        all_content.forEach(content => { content.classList.remove('active') });
         all_content[index].classList.add('active');
     })
+});
 
+$('.menu-icon').on('click', function () {
+    $('.sidebar').css('left', '0px');
+    $('.overlay').css('display', 'block');
+});
+$('.overlay').on('click', function () {
+    $('.sidebar').css('left', '-300px');
+    $('.overlay').css('display', 'none');
 });
